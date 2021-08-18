65e2846340ac2a08683ca40c47ecc3198861c971
Brandon Ly <wowbob396@gmail.com>
2021-08-18 11:39:07 -0500

    added torus to scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-
-
 import { ARCanvas } from '@react-three/xr';
+import { MeshProps } from '@react-three/fiber';
+
+const Torus: React.FC<MeshProps> = props => {
+  const { position } = props;
+  return (
+    <mesh>
+      <torusGeometry position={position} />
+      <meshToonMaterial color="blue" />
+    </mesh>
+  );
+}
+
 
 function App() {
   return (
     <ARCanvas>
-
+      <ambientLight />
+      <pointLight position={[10, 10, 10]}/>
+      <Torus/>
     </ARCanvas>
   );
 }
